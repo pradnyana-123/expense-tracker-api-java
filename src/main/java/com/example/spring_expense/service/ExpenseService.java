93fe41cac4ea8c60dac3ff9e55785ec7cfaae1e5
Andika Pradnyana <andikapradnyana16@gmail.com>
2026-01-28 11:06:06 +0800

    feat(expense): finished createExpense method on expenseService

--- src/main/java/com/example/spring_expense/service/ExpenseService.java
+++ src/main/java/com/example/spring_expense/service/ExpenseService.java
@@ -1,9 +1,16 @@
 package com.example.spring_expense.service;
 
 import com.example.spring_expense.dto.CreateExpenseRequest;
+import com.example.spring_expense.entity.Category;
+import com.example.spring_expense.entity.Expense;
+import com.example.spring_expense.entity.User;
+import com.example.spring_expense.repository.CategoryRepository;
 import com.example.spring_expense.repository.ExpenseRepository;
+import com.example.spring_expense.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ExpenseService {
@@ -12,13 +19,25 @@ public class ExpenseService {
     private ExpenseRepository expenseRepository;
 
     @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private ValidationService validationService;
 
     public void createExpense(CreateExpenseRequest request, Long userId) {
         validationService.validate(request);
 
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
 
+        Expense expense = new Expense();
+        expense.setAmount(request.getAmount());
+        expense.setDescription(request.getDescription());
+        expense.setUser(user);
 
+        expenseRepository.save(expense);
     }
 
 
