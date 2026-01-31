b72edaa53e3db7f2fa1bb8dba7dbe080d3dc3960
Andika Pradnyana <andikapradnyana16@gmail.com>
2026-01-31 16:19:37 +0800

    feat(expense): adding findByUserId method into expense repository

--- src/main/java/com/example/spring_expense/repository/ExpenseRepository.java
+++ src/main/java/com/example/spring_expense/repository/ExpenseRepository.java
@@ -3,5 +3,8 @@ package com.example.spring_expense.repository;
 import com.example.spring_expense.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findByUserId(Long userId);
 }
